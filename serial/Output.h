05a994e966bbc97538964d470a7471825c73fa39
Mohammad Rahimi <mashdimamad@gmail.com>
2017-02-10 22:44:30 -0600

    Add files via upload

---
+++ serial/Output.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   Output.h
+ * Author: amin
+ *
+ * Created on May 11, 2015, 5:09 PM
+ */
+
+#ifndef OUTPUT_H
+#define	OUTPUT_H
+
+#include "Points.h"
+#include "Points_bulk.h"
+#include "Parameter.h"
+
+void output(const Parameter* in, Points_bulk * const pbulk);
+
+#endif	/* OUTPUT_H */
+
